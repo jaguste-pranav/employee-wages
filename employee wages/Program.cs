d663220aa6ededd890567a1e141ba46601369549
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-26 11:19:42 +0530

    [Name] Add. Commit message

---
+++ employee wages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace employee_wages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
