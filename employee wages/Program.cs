9f6fd85e634636f67b56c67ba379835b29ac9934
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-27 21:03:45 +0530

    [pranav] Add. UC2

--- employee wages/Program.cs
+++ employee wages/Program.cs
@@ -7,18 +7,22 @@ namespace employee_wages
         static void Main(string[] args)
         {
             int EMPLOYEE_PRESENT = 1;
+            int HOURLY_WAGE = 20;
+            int totalWages;
+            int WORKING_HOURS = 8;
 
             Random rand = new Random();
             int empAttendance = rand.Next(0, 2);
 
             if (empAttendance == EMPLOYEE_PRESENT)
             {
-                Console.WriteLine("Employee is present");
+                totalWages = HOURLY_WAGE * empAttendance * WORKING_HOURS;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                totalWages = HOURLY_WAGE * empAttendance * WORKING_HOURS;
             }
+            Console.WriteLine("The daily wage is " + totalWages);
         }
     }
 }
