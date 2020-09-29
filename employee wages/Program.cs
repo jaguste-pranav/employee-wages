d459dacbd01ebfa68c40aa22d9eb42143d73cc85
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-09-29 13:47:47 +0530

    [pranav] Add. Switch case added

--- employee wages/Program.cs
+++ employee wages/Program.cs
@@ -6,33 +6,30 @@ namespace employee_wages
     {
         static void Main(string[] args)
         {
-            int EMPLOYEE_PRESENT = 1;
-            int HOURLY_WAGE = 20;
-            int totalWages;
-            int FULL_TIME_WORKING_HOURS = 8;
-            int PART_TIME_WORKING_HOURS = 4;
-            int PART_TIME = 0;
+            int wagePerHour = 20;
+            int empHrs;
+            Random r = new Random();
 
-            Random rand = new Random();
-            int empAttendance = rand.Next(0, 2);
+            int checkEmploymentStatus = r.Next(0, 3);
+            switch (checkEmploymentStatus)
+            {
+                case 0:
+                    empHrs = 0;
+                    Console.WriteLine("The daily wage is " + empHrs * wagePerHour);
+                    break;
+                case 1:
+                    empHrs = 4;
+                    Console.WriteLine("The daily wage is " + empHrs * wagePerHour);
+                    break;
+                case 2:
+                    empHrs = 8;
+                    Console.WriteLine("The daily wage is " + empHrs * wagePerHour);
+                    break;
+                default:
+                    Console.WriteLine("The wage does not exist");
+                    break;
 
-            if (empAttendance == EMPLOYEE_PRESENT)
-            {
-                int employeeStatus = rand.Next(0,2);
-                if(employeeStatus == PART_TIME)
-                {
-                    totalWages = HOURLY_WAGE * empAttendance * PART_TIME_WORKING_HOURS;
-                }
-                else
-                {
-                    totalWages = HOURLY_WAGE * empAttendance * FULL_TIME_WORKING_HOURS;
-                }
             }
-            else
-            {
-                totalWages = HOURLY_WAGE * empAttendance * 0;
-            }
-            Console.WriteLine("The daily wage is " + totalWages);
         }
     }
 }
